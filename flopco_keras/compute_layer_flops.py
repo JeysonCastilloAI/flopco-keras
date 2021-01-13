c18661fabbd9afc163f4380e48b1e794f67da986
JeysonCastilloAI <74440623+JeysonCastilloAI@users.noreply.github.com>
2021-01-12 20:47:04 -0500

    Update compute_layer_flops.py

--- flopco_keras/compute_layer_flops.py
+++ flopco_keras/compute_layer_flops.py
@@ -32,7 +32,7 @@ def compute_conv1d_flops(layer, macs = False):
     flops = w * output_channels * input_channels * w_w
 
     if not macs:
-        flops_bias = numel(layer.output_shape[1]) if layer.use_bias is not None else 0
+        flops_bias = w*output_channels if layer.use_bias is not None else 0
         flops = 2 * flops + flops_bias
         
     return int(flops)
